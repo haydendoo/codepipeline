9172eacb8d9deea309d269b0102368007bde5769
haydendoo <haydenhow@gmail.com>
2025-03-04 10:14:29 +0800

    Fix script

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ echo Pulling docker image...
 docker pull 314146331900.dkr.ecr.ap-southeast-1.amazonaws.com/sigma:latest
 
 echo Running docker
-docker run --name sigma-container -d -p 80:80 314146331900.dkr.ecr.ap-southeast-1.amazonaws.com/sigma:latest
+docker run -d -p 80:80 314146331900.dkr.ecr.ap-southeast-1.amazonaws.com/sigma:latest
 
 echo Successfully ran new container
